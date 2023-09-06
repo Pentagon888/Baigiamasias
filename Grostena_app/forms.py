6c103f85c364e30e5e78cf71a4bb5e15e7662de0
Pentagon888 <gzegozmucin@gmail.com>
2023-09-06 21:39:47 +0300

    Paskutinis darbas.

--- Grostena_app/forms.py
+++ Grostena_app/forms.py
@@ -7,5 +7,31 @@ class AutoServiceForm(forms.ModelForm):
                                                 widget=forms.CheckboxSelectMultiple)
 
     class Meta:
+        error_messages = {
+            "name": {
+                "required": "Šitas laukas yra privalomas.",
+            },
+            "category": {
+                "required": "Šitas laukas yra privalomas.",
+            },
+            "location_lat": {
+                "required": "Šitas laukas yra privalomas.",
+            },
+            "location_lon": {
+                "required": "Šitas laukas yra privalomas.",
+            },
+            "address": {
+                "required": "Šitas laukas yra privalomas.",
+            }, "phone_number": {
+                "required": "Šitas laukas yra privalomas.",
+            },
+            "working_hours": {
+                "required": "Šitas laukas yra privalomas.",
+            },
+            "working_hours": {
+                "required": "Šitas laukas yra privalomas.",
+            }
+
+        }
         model = AutoService
         fields = '__all__'
--- Grostena_app/models.py
+++ Grostena_app/models.py
@@ -14,14 +14,13 @@ class RepairCategory(models.Model):
         return self.name
 
 class AutoService(models.Model):
-    name = models.CharField(max_length=100)
-    category = models.ManyToManyField(RepairCategory, blank=True)
-    location_lat = models.FloatField()
-    location_lon = models.FloatField()
-    address = models.CharField(max_length=200)
-    phone_number = models.CharField(max_length=20)
-    working_hours = models.CharField(max_length=100)
+    name = models.CharField(verbose_name="Vardas",max_length=100)
+    category = models.ManyToManyField(RepairCategory,verbose_name="Kategorija", blank=True)
+    location_lat = models.FloatField(verbose_name="Lokacijos Platuma")
+    location_lon = models.FloatField(verbose_name="Lokacijos Ilguma")
+    address = models.CharField(verbose_name="adresas", max_length=200)
+    phone_number = models.CharField(verbose_name="Telefono numeris", max_length=20)
+    working_hours = models.CharField( verbose_name=" Darbo valandosmax",max_length=100)
 
     def __str__(self):
         return self.name
-
